55870244622a4dde23c2729dd2daffb6e1338132
JustYoussef <mad42bad@gmail.com>
2025-04-17 17:41:37 +0200

    Revert "Delete app/firebase directory"
    This reverts commit 0615d2b7687af3b4a92d076642796e69a59bc8b8.

---
+++ app/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANszhdbCYv9D1qkPcDmqAQEwA4QzsKqdU",
+  authDomain: "laptopia-err404.firebaseapp.com",
+  projectId: "laptopia-err404",
+  storageBucket: "laptopia-err404.firebasestorage.app",
+  messagingSenderId: "360762687482",
+  appId: "1:360762687482:web:6592722349b6967e46ae43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
